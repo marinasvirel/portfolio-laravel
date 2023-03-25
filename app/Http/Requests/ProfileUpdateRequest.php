e76592a0209d12f5e809aea964449e3afb19c3ad
Tizare <matatova-v@mail.ru>
2023-03-25 09:41:26 +0300

    правки в личный кабинет

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,9 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['string', 'max:255'],
+            'surname' => ['string', 'max:255'],
+            'profession' => ['string', 'max:500'],
+            'greeting' => ['string'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
